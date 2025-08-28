448ee1137e2640c499fea7bd953f7eb619091f21
Toure Lassana <l.toure910@laposte.net>
2025-08-28 16:07:06 +0200

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import TodoStats from "./components/TodoStats";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todosss"
+        );
+        const data = await response.json();
+        setTodos(data.slice(0, 5));
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      title: text,
+      completed: false,
+    };
+    setTodos([...todos, { ...newTodo }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const updateTodo = (id, newText) => {
+    setTodos((prev) =>
+      prev.map((todo) => (todo.id === id ? { ...todo, title: newText } : todo))
+    );
+  };
+
+  if (loading) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Todo App - Test Technique</h1>
+        <p>Trouvez et corrigez les bugs !</p>
+      </header>
+
+      <main className="App-main">
+        <TodoForm onAdd={addTodo} />
+
+        <TodoList
+          todos={todos}
+          onToggle={toggleTodo}
+          onDelete={deleteTodo}
+          onUpdate={updateTodo}
+        />
+
+        <TodoStats todos={todos} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
